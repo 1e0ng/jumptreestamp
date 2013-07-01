b5fbe031c74fe389fdee324721a0c4dec431a634
Liang Sun <i@liangsun.org>
2013-07-02 00:14:48 +0800

    Add img into page.

--- static/js/main.js
+++ static/js/main.js
@@ -1,2 +1,17 @@
-var canvas = document.getElementById('my_canvas');
-var ctx = canvas.getContext('2d');
+window.onload = function() {
+    var canvas = document.getElementById('my_canvas');
+    canvas.width = 960;
+    canvas.height = 600;
+
+    var ctx = canvas.getContext('2d');
+    var img = new Image();
+    img.onload = function() {
+    };
+    img.src = 'static/img/treestump.png';
+
+    for (var j = 0; j < 3; ++j) {
+        for (var i = 0; i < 6; ++i) {
+            ctx.drawImage(img, 50 + 150*i, 100 + 150*j + 17*(2.5-i)*(j%2?1:-1));
+        }
+    }
+}
